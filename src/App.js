7399741aea14f9f10407b22785f70bcb4bc49018
Mahesh <mahesh@licious.com>
2020-12-05 17:48:50 +0530

    homeage static changes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
+import HomepageComponent from './pages/homepage/homepage-component'
 
 import './App.css';
-import CardList from './components/card-list/card-list-component'
-import SearchBox from './components/search-box/search-box-component'
 class App extends Component {
 
   constructor(){
     super();
 
     this.state = {
-      monsters:[],
-      searchString : ''
     }
   }
-
-  handleSearch = e =>{
-    this.setState({
-      searchString : e.target.value
-    })
-  }
-
-  componentDidMount(){
-    fetch('http://jsonplaceholder.typicode.com/users').then((resp) => {
-      return resp.json()
-    }).then((users) => {
-      this.setState({
-        monsters:users
-      })
-    })
-  }
-
-
   
   render(){
-    let {monsters, searchString} = this.state
-    monsters = monsters.filter((m) => m.name.toLowerCase().includes(searchString))
-
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
-        <SearchBox placeholder="Search Monsters.." onChange={this.handleSearch}/>
-        <CardList monsters = {monsters} />
+        <HomepageComponent />
       </div>
     );
   }
--- src/components/search-box/search-box-component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './search-box.css';
-
-export default function searchBox(props) {
-    const { placeholder, onChange } = props
-    return (
-        <input className="search-box" type="search" placeholder={placeholder} onChange={onChange} />
-    )
-}
