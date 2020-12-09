1deb4c08d16d90ac0fa9ac0ca4d4ed6778c88de8
Mahesh <mahesh@licious.com>
2020-12-09 17:17:02 +0530

    cart item integrated

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
 import CustomButton from '../custom-button/custom-button';
+import {connect} from 'react-redux'
+import CartItem from '../cart-item/cart-item'
 
 import './cart-dropdown.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
   <div className='cart-dropdown'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+    {
+        cartItems.map((item) => <CartItem key={item.id} item={item} />)
+    }
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+    cartItems : state.cart.cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -11,9 +11,21 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 hidden: !state.hidden
             };
         case 'ADD_ITEM':
+            let itemAlreadyExists = state.cartItems.find((item) => item.id === action.payload.id)
+            if (itemAlreadyExists) {
+                let updatedCartItems = state.cartItems.map(item =>
+                    item.id === action.payload.id
+                        ? { ...item, quantity: item.quantity + 1 }
+                        : item
+                );
+                return {
+                    ...state,
+                    cartItems: updatedCartItems
+                }
+            }
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
+                cartItems: [...state.cartItems, { ...action.payload, quantity: 1 }]
             };
         default:
             return state;
