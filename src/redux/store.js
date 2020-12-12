eccbfcd3912ba0365ab0b0dcf223ad4f6b44ce50
Mahesh <mahesh@licious.com>
2020-12-12 17:07:18 +0530

    logger removed only in prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,8 @@ import {createStore, applyMiddleware} from 'redux'
 import logger from 'redux-logger'
 import rootReducer from './root-reducer'
 
-const middlewares = [logger]
+const middlewares = process.env.NODE_ENV === 'development' ? [logger] : []
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 export default store
-
-
-
