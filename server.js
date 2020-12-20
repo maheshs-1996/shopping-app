836f5ff22cd50e604ada0d9859a94e4b3e072b7e
Mahesh <mahesh@licious.com>
2020-12-20 22:57:58 +0530

    prod PORT error

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ if(process.env.NODE_ENV !== 'production'){
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const app = express()
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
